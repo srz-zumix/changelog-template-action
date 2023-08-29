b249280673c7da8a8ac64efa0b38291eab2f11af
srz_zumix <zumix.cpp@gmail.com>
2023-08-29 21:16:43 +0900

    auto resolve from/to (#3)

--- resolve-from-to.sh
+++ resolve-from-to.sh
@@ -3,6 +3,11 @@ set -euo pipefail
 
 EXIT_CODE=0
 
+if [ "${INPUTS_DEBUG:-false}" = "true" ]; then
+    KAMIDANA_OPTINOS+=(--debug)
+    set -x
+fi
+
 cd "${INPUTS_PATH}"
 
 TARGET_REPO=$(gh repo view --json nameWithOwner --jq .nameWithOwner)
@@ -12,9 +17,18 @@ resolve_to() {
         echo "to=${INPUTS_TO}" >> "${GITHUB_OUTPUT}"
         return
     fi
-    if [ -n "${GITHUB_REF_NAME}" ]; then
-        echo "to=${GITHUB_REF_NAME}" >> "${GITHUB_OUTPUT}"
+    CURRENT_BRANCH=$(git branch --show-current)
+    if [ -n "${CURRENT_BRANCH}" ]; then
+        echo "to=${CURRENT_BRANCH}" >> "${GITHUB_OUTPUT}"
         return
+    fi
+
+    if [ "${TARGET_REPO}" == "${GITHUB_REPOSITORY}" ]; then
+        if [ -n "${GITHUB_REF_NAME}" ]; then
+            git switch -c "${GITHUB_REF_NAME}" 
+            echo "to=${GITHUB_REF_NAME}" >> "${GITHUB_OUTPUT}"
+            return
+        fi
     fi
 
     echo "::error:: Failed to get end of changelog range. Specify it explicitly in \"to\" inputs."
@@ -27,13 +41,20 @@ resolve_from() {
         return
     fi
 
+    PREV_RELEASE_TAG_NAME=
     if [ "${GITHUB_EVENT_NAME}" == "release" ]; then
         TARGET_COMMITISH=$(jq -r '.release.target_commitish' < "${GITHUB_EVENT_PATH}")
-        PREV_RELEASE_TAG_NAME=$(gh api "/repos/${TARGET_REPO}/releases" --jq ".[] | select(.target_commitish == '${TARGET_COMMITISH}') | .tag_name" | head -2 | tail -1)
+        PREV_RELEASE_TAG_NAME=$(gh api "/repos/${TARGET_REPO}/releases" --jq ".[] | select(.target_commitish == \"${TARGET_COMMITISH}\") | .tag_name" | head -2 | tail -1)
     else
-        if [ -n "${GITHUB_REF_NAME}" ]; then
-            TARGET_COMMITISH="${GITHUB_REF_NAME}"
-            PREV_RELEASE_TAG_NAME=$(gh api "/repos/${TARGET_REPO}/releases" --jq ".[] | select(.target_commitish == '${TARGET_COMMITISH}') | .tag_name" | head -1)
+        if [ "${TARGET_REPO}" == "${GITHUB_REPOSITORY}" ]; then
+            TARGET_COMMITISH_LIST=("${GITHUB_BASE_REF}" "refs/heads/${GITHUB_BASE_REF}" "${GITHUB_REF_NAME}" "${GITHUB_REF}")
+            for TARGET_COMMITISH in "${TARGET_COMMITISH_LIST[@]}"; do
+                if [ -n "${TARGET_COMMITISH}" ] && [ -z "${PREV_RELEASE_TAG_NAME}" ]; then
+                    PREV_RELEASE_TAG_NAME=$(gh api "/repos/${TARGET_REPO}/releases" --jq ".[] | select(.target_commitish == \"${TARGET_COMMITISH}\") | .tag_name" | head -1)
+                fi
+            done
+        else
+            PREV_RELEASE_TAG_NAME=$(gh api "/repos/${TARGET_REPO}/releases/latest" --jq ".tag_name")
         fi
     fi
 
