ab887bacec3781a25a3c0671492548b4102df3cb
srz_zumix <zumix.cpp@gmail.com>
2023-08-29 18:49:01 +0900

    up

--- resolve-from-to.sh
+++ resolve-from-to.sh
@@ -38,6 +38,7 @@ resolve_from() {
         return
     fi
 
+    PREV_RELEASE_TAG_NAME=
     if [ "${GITHUB_EVENT_NAME}" == "release" ]; then
         TARGET_COMMITISH=$(jq -r '.release.target_commitish' < "${GITHUB_EVENT_PATH}")
         PREV_RELEASE_TAG_NAME=$(gh api "/repos/${TARGET_REPO}/releases" --jq ".[] | select(.target_commitish == \"${TARGET_COMMITISH}\") | .tag_name" | head -2 | tail -1)
